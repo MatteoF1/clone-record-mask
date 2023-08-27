3498adc4e056ca7eccfaa7e7018e1c2c940ab41b
Matteo Facchini <matteo.facchini@outlook.com>
2023-08-27 19:32:55 +0200

    mask is working

--- force-app/main/default/lwc/cloneRecordMaskForScreenFlow/cloneRecordMaskForScreenFlow.js
+++ force-app/main/default/lwc/cloneRecordMaskForScreenFlow/cloneRecordMaskForScreenFlow.js
@@ -1,5 +1,5 @@
-import { LightningElement, api, wire } from 'lwc';
-import { createRecord, generateRecordInputForCreate , getRecordCreateDefaults} from 'lightning/uiRecordApi';
+import { LightningElement, api, wire, track } from 'lwc';
+import { getRecord , getRecordCreateDefaults} from 'lightning/uiRecordApi';
 import { FlowNavigationBackEvent, FlowNavigationNextEvent } from 'lightning/flowSupport';
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
@@ -10,56 +10,90 @@ export default class CloneRecordMaskForScreenFlow extends LightningElement {
     @api recordTypeId;
     @api newRecordId;
 
+    @track layoutSections;
+
     @wire(getRecordCreateDefaults, { objectApiName: '$objectApiName' , recordTypeId : '$recordTypeId'})
-    recordCreateDefaults({error, data}){
+    retrieveObjectInfoForNewRecord({error, data}){
         if(data){
-            const recordObjectInfo = data.objectInfos[this.objectApiName];
-            const recordDefaults = data.record;
-            this.recordInput = generateRecordInputForCreate(recordDefaults, recordObjectInfo);
+            this._defaultFieldsFromRecordType = data.record.fields;
+            this._layoutSections = data.layout.sections;
+            this._prepareDefaultsAndLayoutSectionsForDisplay();
         }
-        else{
+        else if(error){
             console.log('Error fetching defaults: ', JSON.stringify(error));
         }
     };
 
-    handleSubmit(event) {
-        event.preventDefault(); 
-        const userSelectedFields = event.detail.fields;
-        Object.assign(this.recordInput.fields, userSelectedFields);
-        createRecord(this.recordInput).then(data => {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                  title: "Success!",
-                  message: 'Record created successfully!',
-                  variant: "success",
-                }),
-            );
-            console.log(JSON.stringify(data.id));
-            this.newRecordId = data.id;
-            const navigateNextEvent = new FlowNavigationNextEvent();
-            this.dispatchEvent(navigateNextEvent);
-        }).catch(error => {
-            let errorMessage = error.message;
-            try{
-                errorMessage = error.body.output.errors[0].message
-            }
-            catch(errorNoSFinternalError){
-                console.log('A generic error happened.');
-            }
+    @wire(getRecord, {recordId: '$recordId', layoutTypes: 'Full'})
+    retrieveExistingRecordData({error, data}){
+        if(data){
+            this._defaultFieldsFromExistingRecord = data.fields;
+            this._prepareDefaultsAndLayoutSectionsForDisplay();
+        }
+        else if(error){
+            console.log('Error fetching defaults: ', JSON.stringify(error));
+        }
+    };
 
-            this.dispatchEvent(
-                new ShowToastEvent({
-                  title: "Error!",
-                  message: errorMessage,
-                  variant: "error",
-                }),
-            );
-        });
+    handleSuccess(event){
+        this.dispatchEvent(
+            new ShowToastEvent({
+              title: "Success!",
+              message: 'Record created successfully!',
+              variant: "success",
+            }),
+        );
+        this.newRecordId = event.detail.id;
+        const navigateNextEvent = new FlowNavigationNextEvent();
+        this.dispatchEvent(navigateNextEvent);
+    }
+
+    handleError(event){
+        console.log(JSON.stringify(event));
+        this.dispatchEvent(
+            new ShowToastEvent({
+              title: "Error!",
+              message: errorMessage,
+              variant: "error",
+            }),
+        );
     }
 
     handleCancel(event) {
         event.preventDefault(); 
         const navigateBackEvent = new FlowNavigationBackEvent();
         this.dispatchEvent(navigateBackEvent);
+    }
+
+    _prepareDefaultsAndLayoutSectionsForDisplay(){
+        if(this._defaultFieldsFromRecordType && this._defaultFieldsFromExistingRecord && this._layoutSections){
+            const defaultFields = Object.assign({selected: false}, this._defaultFieldsFromRecordType, this._defaultFieldsFromExistingRecord);
+            this.layoutSections = this._prepareLayoutSectionsForDisplay(this._layoutSections, defaultFields);
+        }
+    }
+
+    // this requires refactoring because it can be improved
+    _prepareLayoutSectionsForDisplay(sections, defaultFields){
+        const layoutSections = sections.map((section, indexSection) => { 
+            const newSection= Object.assign({selected: false}, section);
+            newSection['layoutRows'] = section.layoutRows.map(
+                (row, indexRow) => {
+                    const newRow = Object.assign({selected: false}, row);
+                    newRow['rowKey'] = indexSection + '-' + indexRow;
+                    newRow.layoutItems = row.layoutItems.map((item, indexItem) => {
+                        const newItem = Object.assign({selected: false}, item);
+                        newItem['itemKey'] = indexSection + '-' + indexRow + '-' + indexItem;
+                        const newLayoutComponent = Object.assign({selected: false}, item.layoutComponents[0]);
+                        if(newLayoutComponent && newLayoutComponent.apiName){
+                            newLayoutComponent.value = defaultFields[newLayoutComponent.apiName].value;
+                        }
+                        newItem['layoutComponents'] = [newLayoutComponent];
+                        return newItem;
+                    });
+                    return newRow;
+            });
+            return newSection;
+        });
+        return layoutSections;
     }
 }
